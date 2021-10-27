1f8a5095f50161b66242d1a3c2629ca1b0a05609
Nathan Andrews <nathoandrews@gmail.com>
2021-10-27 20:58:13 +0100

    Run class where reversing of Strirng takes place, recursive and a loop without the use of a library

---
+++ StringReverse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MastercardApp;
+
+/**
+ *
+ * @author Nathan
+ */
+public class StringReverse {
+    public static void main(String[] args){
+        String message = "Mastercard";
+        
+        String reversed = Strings.reverseString(message);
+        System.out.println(message + " reversed without recurssion " + reversed);
+        
+        String recursiveReverse = Strings.recursiveReverse(message);
+        System.out.println("This is the same String but done recurssively " + recursiveReverse);
+    }
+}
